fef5da1f65f2abd3e2f76695b178182f16ea699d
BillMagic <mqyznb@gmail.com>
2015-07-16 00:09:12 -0400

    add mapbox

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   get "portfolio" => "pages#_home"
   get "about" => "pages#about"
   get "demo2men" => "pages#demo2men"
+  get "demo1men" => "pages#demo1men"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
